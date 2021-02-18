fc61978d0ecd68167037b181ae00259510d5ba15
Orlando Murcio <ato.murcio@gmail.com>
2021-02-18 15:09:39 -0700

    Crreate question's data

--- src/components/UseCase/UseCase.js
+++ src/components/UseCase/UseCase.js
@@ -3,11 +3,34 @@ import Questionnaire from '../Questionnaire'
 import './UseCase.css'
 
 const UserCase = () => {
-  
+
+  const questions = [
+    {
+      category: 'Target',
+      description: 'Who is going to be using the following use case'
+    },
+    {
+      category: 'Company type' ,
+      description: 'Where is the following product will be use, e.g law firms, inhouse legal departments'
+    },
+    {
+      category: 'Product name' ,
+      description: 'Plese insert the name of the product'
+    },
+    {
+      category: 'Action',
+      description: 'Describe an event for the use of the product, perhaps an app action'
+    },
+    {
+      category: 'Task',
+      description: 'Describe the task the product helps the end-use with / the ise case for the product'
+    }
+  ]
+
   return (
 
     <div className="UseCase">
-      <Questionnaire/>
+      <Questionnaire questions={questions}/>
     </div>
   )
 }
