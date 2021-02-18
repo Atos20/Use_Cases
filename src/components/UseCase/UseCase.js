584bfd891a3c7ef76ca18c89d38696f2837a035c
Orlando Murcio <ato.murcio@gmail.com>
2021-02-18 13:52:18 -0700

    Remove commments

--- src/components/UseCase/UseCase.js
+++ src/components/UseCase/UseCase.js
@@ -1,47 +1,13 @@
 import React from 'react'
-import { Formik, Field, Form } from 'formik'
+import Questionnaire from '../Questionnaire'
+import './UseCase.css'
 
 const UserCase = () => {
-//state = useState({})
-/*
-data requires 5 entries???
-data = {
-  1   'target '    : 'user input', => select drop down
-  2   'company type'   : 'user input', => select drop down
-  3   'product name'     : 'user input', =>  user input 
-  4   'action or event '    : 'user input', => user input
-  5   'task to solve'    :'user input' => user input 
-}
-*/
-
-
-  // Use Formik
-  //hook to get the information //check docs
-
-  //
-  //  create a controlled form 
-  // string validation => making sure it is not empty
-  // gather the data in the state
-
-
-  // witht state SUbmit Fn
+  
   return (
 
-    <div className="App">
-      <h1>Contact Us</h1>
-      <Formik
-        initialValues={{ name: "", email: "" }}
-        onSubmit={async values => {
-          await new Promise(resolve => setTimeout(resolve, 500));
-          alert(JSON.stringify(values, null, 2));
-        }}
-      >
-        <Form>
-          <Field name="name" type="text" />
-          <Field name="email" type="email" />
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
+    <div className="UseCase">
+      <Questionnaire/>
     </div>
   )
 }
